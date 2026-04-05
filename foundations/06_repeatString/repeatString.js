373573cccbfdc7fd633a45e29c7bc32d366f8214
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-04-06 00:32:04 +0200

    Complete exercise 06: Repeat Strings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, loops) {
+    if (loops >= 0) {
+        let base = "";
+        for (let i = 0; i < loops; i++) {
+            base += string;
+        }
+        return base;
+    }
+    return "ERROR";
 };
-
 // Do not edit below this line
 module.exports = repeatString;
