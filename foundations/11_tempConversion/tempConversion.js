2a4809f82af8d89d75822ad77e74b2de26de4c53
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-04-08 04:30:34 +0200

    Complete exercise 11: Temperature Conversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return Math.round(((temperature - 32) / 1.8) * 10) / 10; // .round() rounds to the nearest integer, while .floor() rounds down
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return Math.round(((temperature * 1.8) + 32) * 10) / 10;
 };
-
+console.log(convertToCelsius(-100))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
