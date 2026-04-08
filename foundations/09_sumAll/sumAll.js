eafb158787145f155a56647637902fe0305d292a
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-04-08 04:29:25 +0200

    Complete exercise 09: Sum All

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
+    if(start > end){
+        [start,end] = [end, start];
+    }
+    let sum = start;
+    while(start<end){
+        ++start;
+        sum += start;
+    }
+    return sum;
 };
-
+console.log(sumAll(1,"hey"));
 // Do not edit below this line
 module.exports = sumAll;
