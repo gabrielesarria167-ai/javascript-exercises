2189668de78e87843f0256d332d50a0a4948666f
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-04-08 04:27:51 +0200

    Complete exercise 08: Remove from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeValue) { // spread syntax creates a list of values, you cant compare a singular element to a list
+    return array.filter((element) => !removeValue.includes(element));
 };
-
+console.log(removeFromArray(["heyy",2,3,4,5], "heyy"))
 // Do not edit below this line
 module.exports = removeFromArray;
